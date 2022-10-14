e772b6f5e050a7f1ea51a694e75efbc66049a534
sbiswal <sumeet_biswal@spe.sony.com>
2022-10-14 21:47:31 +0530

    initial commit

--- modules/custom/employee/src/Model/EmployeeModel.php
+++ modules/custom/employee/src/Model/EmployeeModel.php
@@ -3,125 +3,140 @@
 namespace Drupal\employee\Model;
 
 use Drupal\Core\Controller\ControllerBase;
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Database\Connection;
 use Drupal\library\Lib\DataModel;
 
 class EmployeeModel extends ControllerBase  {
-	
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  private $connection;
+
+  /**
+   * @param \Drupal\Core\Database\Connection $connection
+   *  The database connection.
+   */
+  public function __construct(Connection $connection) {
+        $this->connection = $connection;
+  }
+
 	public function createUser($userdata)
 	{
-		
+
 		$user = \Drupal\user\Entity\User::create();
-			
+
 		//Mandatory.
 		$user->setUsername($userdata['username']);
 		$user->setPassword($userdata['password']);
 		$user->setEmail($userdata['email']);
 		//$user->enforceIsNew();
-		
+
 		/* Drupal wont allow to set authenticated role manually as it is bydefault */
 		if( $userdata['role'] != 'authenticated')
 		{
-			$user->addRole($userdata['role']); 
-		} 
+			$user->addRole($userdata['role']);
+		}
 		//optional
 		$language = 'en';
 		$user->set("init", $userdata['email']);
 		$user->set("langcode", $language);
 		$user->set("preferred_langcode", $language);
-		$user->set("preferred_admin_langcode", $language); 
+		$user->set("preferred_admin_langcode", $language);
 		$user->activate();
-		
+
 		if(isset($userdata['image']))
 		{
-			$user->set('user_picture', $userdata['image']); 
-		} 
-		
-		
+			$user->set('user_picture', $userdata['image']);
+		}
+
+
 		 //Save user account.
 		 $user->save();
-		 
+
 		 return $user;
 	}
 
 	public static function setPersonalInfo($user, $data, $opt, &$context )
 	{
 		//$context['message'] = "Now processing word...";
-		$query = \Drupal::database();	
-		
+		$query = \Drupal::database();
+
 		$query->query('start transaction');
-		
-		//personal info 
-		$query->query('insert into '.DataModel::EMPPERSONAL.' set userpk = :userpk, firstname = :fname, lastname= :lname, fathername= :fthname, mothername= :mthname, gender= :gender, dob = :dob,marital= :marital, bloodgroup= :blood, religion= :religion, nationality= :nationality', 
+
+		//personal info
+		$query->query('insert into '.DataModel::EMPPERSONAL.' set userpk = :userpk, firstname = :fname, lastname= :lname, fathername= :fthname, mothername= :mthname, gender= :gender, dob = :dob,marital= :marital, bloodgroup= :blood, religion= :religion, nationality= :nationality',
 		array(':userpk'=>$user->Id(), ':fname'=>$data['personal']['firstname'], ':lname'=>$data['personal']['lastname'], ':fthname'=>$data['personal']['fname'], ':mthname'=>$data['personal']['mname'], ':gender'=>$data['personal']['gender'], ':dob'=>$data['personal']['dob'], ':marital'=>$data['personal']['marital'], ':blood'=>$data['personal']['blood'],':religion'=>$data['personal']['religion'],':nationality'=>$data['personal']['nationality']));
-		
+
 		//contact info
-		$query->query('insert into '.DataModel::EMPCONTACT.' set userpk = :userpk, phoneno = :phoneno, altphoneno= :altphoneno, emrgphoneno= :emrgphoneno, relationship= :relationship, email= :email, res_address1= :res_address1, res_address2= :res_address2, res_state= :res_state, res_city= :res_city, res_country= :res_country, res_pincode= :res_pincode, perm_address1= :perm_address1, perm_address2= :perm_address2, perm_state= :perm_state, perm_city= :perm_city, perm_country= :perm_country, perm_pincode= :perm_pincode, status= :status', 
-		array(':userpk'=>$user->Id(), ':phoneno'=>$data['contact']['phoneno'], ':altphoneno'=>$data['contact']['altphoneno'], ':emrgphoneno'=>$data['contact']['emergencyno'], ':relationship'=>$data['contact']['relationship'], ':email'=>$data['contact']['email'], ':res_address1'=>$data['contact']['address1'], 
+		$query->query('insert into '.DataModel::EMPCONTACT.' set userpk = :userpk, phoneno = :phoneno, altphoneno= :altphoneno, emrgphoneno= :emrgphoneno, relationship= :relationship, email= :email, res_address1= :res_address1, res_address2= :res_address2, res_state= :res_state, res_city= :res_city, res_country= :res_country, res_pincode= :res_pincode, perm_address1= :perm_address1, perm_address2= :perm_address2, perm_state= :perm_state, perm_city= :perm_city, perm_country= :perm_country, perm_pincode= :perm_pincode, status= :status',
+		array(':userpk'=>$user->Id(), ':phoneno'=>$data['contact']['phoneno'], ':altphoneno'=>$data['contact']['altphoneno'], ':emrgphoneno'=>$data['contact']['emergencyno'], ':relationship'=>$data['contact']['relationship'], ':email'=>$data['contact']['email'], ':res_address1'=>$data['contact']['address1'],
 		':res_address2'=>$data['contact']['address2'],':res_state'=>$data['contact']['state'], ':res_city'=>$data['contact']['city'], ':res_country'=>$data['contact']['country'], ':res_pincode'=>$data['contact']['pincode'], ':perm_address1'=>$data['contact']['permanentaddress1'], ':perm_address2'=>$data['contact']['permanentaddress2'], ':perm_state'=>$data['contact']['permanentstate'], ':perm_city'=>$data['contact']['permanentcity'], ':perm_country'=>$data['contact']['permanentcountry'], ':perm_pincode'=>$data['contact']['permanentpincode'], ':status'=> 1));
-		
+
 		//academic info
 		foreach($data['qualification'] AS $qual)
 		{
-			$query->query('insert into '.DataModel::EMPACADEMIC.' set userpk = :userpk, class = :class, stream= :stream, board= :board, yearofpassing= :yearofpassing, score = :score', 
+			$query->query('insert into '.DataModel::EMPACADEMIC.' set userpk = :userpk, class = :class, stream= :stream, board= :board, yearofpassing= :yearofpassing, score = :score',
 			array(':userpk'=>$user->Id(), ':class'=>$qual['class'], ':stream'=>$qual['stream'], ':board'=>$qual['university'], ':yearofpassing'=>$qual['yearofpassing'], ':score'=>$qual['score']));
-		
+
 		}
-		
+
 		//employeement info
 		foreach($data['experience'] AS $qual)
 		{
-			$query->query('insert into '.DataModel::EMPEXPRNC.' set userpk = :userpk, organisation = :organisation, designation= :designation, fromdate= :fromdate, todate= :todate', 
+			$query->query('insert into '.DataModel::EMPEXPRNC.' set userpk = :userpk, organisation = :organisation, designation= :designation, fromdate= :fromdate, todate= :todate',
 			array(':userpk'=>$user->Id(),':organisation'=>$qual['organisation'], ':designation'=>$qual['designation'], ':fromdate'=>$qual['fromdate'], ':todate'=>$qual['todate']));
-		
+
 		}
-		
+
 		//official info
-		$query->query('insert into '.DataModel::EMPOFFICIAL.' set userpk = :userpk, empid = :empid, department= :department, branch= :branch, designation= :designation, jobnature = :jobnature, email= :email, doj= :doj, jobtype= :jobtype, shifttime= :shifttime', 
+		$query->query('insert into '.DataModel::EMPOFFICIAL.' set userpk = :userpk, empid = :empid, department= :department, branch= :branch, designation= :designation, jobnature = :jobnature, email= :email, doj= :doj, jobtype= :jobtype, shifttime= :shifttime',
 		array(':userpk'=>$user->Id(), ':empid'=>$data['official']['id'], ':department'=>$data['official']['department'], ':branch'=>$data['official']['branch'], ':designation'=>$data['official']['designation'], ':jobnature'=>$data['official']['jobnature'], ':email'=>$data['official']['officialemail'], ':doj'=>$data['official']['doj'], ':jobtype'=>$data['official']['jobtype'],':shifttime'=>$data['official']['shifttime']));
-		
+
 		$query->query('commit');
-		
+
 	}
-	
+
 	public static function finishOperation()
   {
 	  $response = new RedirectResponse(\Drupal\Core\Url::fromRoute('employee.emplist')->toString());
 	  $response->send();
 	  return;
     }
-	
+
 	public function checkUserIdExist($username)
 	{
 		$query = db_select(DataModel::USERDATA, 'name');
-				$query->fields('name');	
+				$query->fields('name');
 				$query->condition('status', 1, "=");
 				$query->condition('name', $username , "=");
 				$result = $query->execute()->fetch();
-		
-		$res = $result;	
+
+		$res = $result;
 		return $res;
 	}
-	
+
 	public function checkEMailIdExist($email)
 	{
 		$query = db_select(DataModel::USERDATA, 'mail');
-				$query->fields('mail');	
+				$query->fields('mail');
 				$query->condition('status', 1, "=");
 				$query->condition('mail', $email , "=");
 				$result = $query->execute()->fetch();
-		
-		$res = $result;	
+
+		$res = $result;
 		return $res;
 	}
-	
+
 	public function getPersonalDetailsById($id)
 	{
-		$query = db_select(DataModel::EMPPERSONAL, 'n');
-		$query->fields('n');	
+		$query = $this->connection->select(DataModel::EMPPERSONAL, 'n');
+		$query->fields('n');
 		$query->condition('userpk', $id ,"=");
 		$result = $query->execute()->fetch();
 		return $result;
@@ -132,7 +147,7 @@ class EmployeeModel extends ControllerBase  {
 	*/
 	public function getOfficialDetailsById($id)
 	{
-		/*SELECT 
+		/*SELECT
 			oi.empid AS employeeid,
 			cv1.codevalues AS branch,
 			cv2.codevalues AS department,
@@ -160,29 +175,29 @@ class EmployeeModel extends ControllerBase  {
 		$query->leftjoin(DataModel::CODEVAL, 'cv4', 'cv4.codename = oi.jobtype AND cv4.codetype = :jbtpy', array('jbtpy'=>'jobtype'));
 		$query->leftjoin(DataModel::CODEVAL, 'cv5', 'cv5.codename = oi.jobnature AND cv5.codetype = :jbntr', array('jbntr'=>'jobnature'));
 		$query->leftjoin(DataModel::CODEVAL, 'cv6', 'cv6.codename = oi.shifttime AND cv6.codetype = :jbshft', array('jbshft'=>'jobshift'));
-		$query->fields('oi', array('empid'));	
-		$query->addField('cv1', 'codevalues', 'branch');	
-		$query->addField('cv2', 'codevalues', 'department');	
-		$query->addField('cv3', 'codevalues', 'designation');	
-		$query->addField('cv4', 'codevalues', 'jobtype');	
-		$query->addField('cv5', 'codevalues', 'jobnature');	
-		$query->addField('oi', 'email', 'email');	
-		$query->addField('oi', 'doj', 'joining');	
-		$query->addField('cv6', 'codevalues', 'jobshift');			
+		$query->fields('oi', array('empid'));
+		$query->addField('cv1', 'codevalues', 'branch');
+		$query->addField('cv2', 'codevalues', 'department');
+		$query->addField('cv3', 'codevalues', 'designation');
+		$query->addField('cv4', 'codevalues', 'jobtype');
+		$query->addField('cv5', 'codevalues', 'jobnature');
+		$query->addField('oi', 'email', 'email');
+		$query->addField('oi', 'doj', 'joining');
+		$query->addField('cv6', 'codevalues', 'jobshift');
 		$query->condition('oi.userpk', $id ,"=");
-		
+
 		$result = $query->execute()->fetch();
 		return $result;
 	}
-	
+
 	/*
 	* @parameter user id
-	* get contact details 
+	* get contact details
 	*/
 	public function getContactDetailsById($id)
 	{
 		/*
-		SELECT 
+		SELECT
 			cnt.phoneno AS 'phone',
 			cnt.altphoneno AS 'altphone',
 			cnt.emrgphoneno AS 'emrgphone',
@@ -217,8 +232,8 @@ class EmployeeModel extends ControllerBase  {
 		$query->leftjoin(DataModel::STATE, 'pst', 'pst.id = cnt.perm_state');
 		$query->leftjoin(DataModel::CITY, 'pct', 'pct.id = cnt.perm_city');
 		$query->leftjoin(DataModel::COUNTRY, 'pcntry', 'pcntry.id = cnt.perm_country');
-		
-		$query->fields('cnt', array('phoneno'));		
+
+		$query->fields('cnt', array('phoneno'));
 		$query->addField('cnt', 'altphoneno', 'altphone');
 		$query->addField('cnt', 'emrgphoneno', 'emrgphone');
 		$query->addField('cnt', 'relationship', 'relationship');
@@ -235,52 +250,52 @@ class EmployeeModel extends ControllerBase  {
 		$query->addField('pct', 'name', 'perm_city');
 		$query->addField('pcntry', 'name', 'perm_country');
 		$query->addField('cnt', 'perm_pincode', 'perm_pincode');
-		
+
 		$query->condition('cnt.userpk', $id ,"=");
 		$result = $query->execute()->fetch();
 		return $result;
-		
+
 	}
-	
+
 	public function getAcademicDetailsById($id)
 	{
 	 	$query = db_select(DataModel::EMPACADEMIC, 'n');
-		$query->fields('n');	
+		$query->fields('n');
 		$query->condition('userpk', $id ,"=");
 		$result = $query->execute()->fetchAll();
 		return $result;
 	}
-	
+
 	public function getPrevEmployeementDetailsById($id)
 	{
 		$query = db_select(DataModel::EMPEXPRNC, 'n');
-		$query->fields('n');	
+		$query->fields('n');
 		$query->condition('userpk', $id ,"=");
 		$result = $query->execute()->fetchAll();
 		return $result;
 	}
-	
+
 	public function getEmployeeList()
 	{
 		$query = db_select(DataModel::EMPPERSONAL, 'n');
 		$query -> innerJoin(DataModel::EMPOFFICIAL, 'nf','n.userpk = nf.userpk');
 		$query->orderBy('n.createdon', 'DESC');
-		$query->fields('n');	
-		$query->fields('nf');	
+		$query->fields('n');
+		$query->fields('nf');
 		$result = $query->execute()->fetchAll();
 		return $result;
-    
+
 	}
 
 	public function getEmployeeCount()
 	{
 		$query = db_select(DataModel::EMPPERSONAL, 'n');
 		$query->condition('status', 1, "=");
-		$query->fields('n');	
+		$query->fields('n');
 		$result = $query->execute()->fetchAll();
 		return count($result);
 	}
-	
+
 	/*
 	* get user pic from Drupal user object
 	* Set default Pic if user has not uploaded pic
@@ -289,28 +304,28 @@ class EmployeeModel extends ControllerBase  {
 	{
 		$user = \Drupal::currentUser();
 		$personal_details = $this->getPersonalDetailsById($user->id());
-		
+
 		$userobj = \Drupal::service('entity.manager')->getStorage('user')->load($user->id());
 		$avatar = 'male.jpg';
 		if($userobj->user_picture->entity != NULL)
 		{
-		  $avatar = $userobj->user_picture->entity->getFileName();     
+		  $avatar = $userobj->user_picture->entity->getFileName();
 		}
 		else
 		{
-		  $avatar = ( $personal_details->gender == 'M' ) ? 'male.jpg' : 'female.jpg';	  
+		  $avatar = ( $personal_details->gender == 'M' ) ? 'male.jpg' : 'female.jpg';
 		}
-		
+
 		return  $avatar;
 	}
-	/* 
+	/*
 	* joining personalinfo, Contactinfo and Officialinfo
 	* Fetching all Employee details except Academic Info
-    */	
+    */
 	public function getEmployeeDetails()
 	{
 		/*
-			    SELECT 
+			    SELECT
 				   p.firstname,
 				   p.lastname,
 				   p.fathername,
@@ -353,11 +368,11 @@ class EmployeeModel extends ControllerBase  {
 					LEFT JOIN srch_states s ON c.res_state = s.id
 					LEFT JOIN srch_cities ct ON c.res_city = ct.id
 					LEFT JOIN srch_countries co ON c.res_country = co.id
-					LEFT JOIN srch_states s1 ON c.perm_state = s1.id 
-					LEFT JOIN srch_cities ct1 ON c.perm_city = ct1.id 
+					LEFT JOIN srch_states s1 ON c.perm_state = s1.id
+					LEFT JOIN srch_cities ct1 ON c.perm_city = ct1.id
 					LEFT JOIN srch_countries co1 ON c.res_country = co1.id
 					LEFT JOIN srch_officialinfo o ON o.userpk = c.userpk
-					
+
 				;
 		*/
 		$query = db_select(DataModel::EMPPERSONAL, 'p');
@@ -370,7 +385,7 @@ class EmployeeModel extends ControllerBase  {
 		$query->leftjoin(DataModel::COUNTRY, 'co1', 'c.res_country = co1.id');
 		$query->leftjoin(DataModel::EMPOFFICIAL, 'o', 'o.userpk = c.userpk');
 
-		
+
 		$query->addField('p', 'firstname', 'FirstName');
 		$query->addField('p', 'lastname', 'LastName');
 		$query->addField('p', 'fathername', 'FatherName');
