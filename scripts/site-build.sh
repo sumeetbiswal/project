00b5d0753394686091fbb0a25dd3463d3f7af0ea
SBiswal <Sumeet_Biswal@spe.sony.com>
2024-03-07 00:33:49 +0530

    updated deploy scripting file

--- scripts/site-build.sh
+++ scripts/site-build.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+# First running x-debug off command
+rm /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini && kill -USR2 $(pgrep -o php-fpm) > /dev/null || /etc/init.d/apache2 reload
+tput setaf 1 && echo "Xdebug switched Off" && tput sgr 0 && echo
+
 echo "Xsmind site build started..."
 echo "Dropping Schema..."
 
-drush sql-drop --yes
+php /app/vendor/drush/drush/drush sql-drop --yes
 echo "Importing xsmind.sql file."
-drush sql-cli < database/xsmind.sql
+php /app/vendor/drush/drush/drush sql-cli < database/xsmind.sql
 echo "Database Imported..."
 
 
 echo ""
 echo "Clearing Cache.... please wait !"
-drush cache-rebuild
+php /app/vendor/drush/drush/drush cache-rebuild
 
 
 echo ""
 echo "importing configuration."
-drush config-import --yes
+php /app/vendor/drush/drush/drush config-import --yes
 
 
 echo ""
 echo "Updating DB..."
-drush updatedb --yes
+php /app/vendor/drush/drush/drush updatedb --yes
 
 
 echo "Clearing Cache"
-drush cache-rebuild
+php /app/vendor/drush/drush/drush cache-rebuild
