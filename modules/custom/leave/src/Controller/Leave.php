95acb073b0b5b98fc2d4af71b1bc1af10befe92f
SBiswal <Sumeet_Biswal@spe.sony.com>
2024-05-05 10:36:51 +0530

    leave type listing as a view table

--- modules/custom/leave/src/Controller/Leave.php
+++ modules/custom/leave/src/Controller/Leave.php
@@ -14,30 +14,6 @@ class Leave extends ControllerBase {
    */
   public function leavelist() {
 
-    $rows = [];
-    $element['display']['Leavelist'] = [
-      '#type'         => 'table',
-      '#header'       => [
-        $this->t('Request Type.'),
-        $this->t('Leave request'),
-        $this->t('Leave type'),
-        $this->t('Start date'),
-        $this->t('End date'),
-        $this->t('Number of days'),
-        $this->t('Status'),
-        $this->t('Submit date'),
-      ],
-      '#rows'            => $rows,
-      '#attributes' => [
-        'class' => [
-          'table text-center table-hover table-striped table-bordered dataTable',
-        ],
-        'style' => ['text-align-last: center;'],
-      ],
-      '#empty'        => 'No Leaves applied yet.',
-    ];
-    return $element;
-
   }
 
 }
